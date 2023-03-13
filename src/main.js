4c764d6d750b19dc584e781959ec243b1a53699d
AioChyan <1751447344@qq.com>
2023-03-13 09:30:03 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+//导入全局css
+import './assets/css/global.css'
+//导入axios
+import axios from 'axios'
+//配置请求的根路径
+axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/';
+Vue.prototype.$http=axios
 
 Vue.config.productionTip = false
 
