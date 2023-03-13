99b691dee0fe410af1001066adeee3f2d2c70131
AioChyan <1751447344@qq.com>
2023-03-13 17:24:12 +0800

    完成用户列表的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,148 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout"> 退出</el-button>
-  </div>
+  <el-container>
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout"> 退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+        >
+          <!-- 一级菜单 -->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menulist"
+            :key="item.id"
+          >
+            <!-- 一级菜单的模版区域 -->
+            <template slot="title">
+              <!-- 图表区域 -->
+              <i class="el-icon-s-custom"></i>
+              <!-- 文本区域 -->
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+            >
+              <!-- 二级菜单的模版区域 -->
+              <template slot="title">
+                <!-- 图表区域 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本区域 -->
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
+
+
 
 <script>
 export default {
+  data() {
+    return {
+      menulist: [],
+      //是否折叠
+      isCollapse: false,
+      userlist: [],
+      total: 0,
+    }
+  },
+  created() {
+    this.getMenuList()
+  },
+
   methods: {
+    //退出
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+
+    //获取所有菜单
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      console.log(res)
+      if (res.meta.status != 200) {
+        return this.$message.error(res.meta.msg)
+      }
+      this.menulist = res.data
+    },
+    //点击按钮，切换菜单的折叠与展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
     },
   },
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0%;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 18px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -7,7 +7,12 @@ import './assets/css/global.css'
 //导入axios
 import axios from 'axios'
 //配置请求的根路径
-axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/';
+axios.defaults.baseURL = 'https://lianghj.top:12969/api/private/v1/';
+axios.interceptors.request.use( config => {
+  console.log(config)
+  config.headers.Authorization= window.sessionStorage.getItem('token')
+  return config;
+})
 Vue.prototype.$http=axios
 
 Vue.config.productionTip = false
